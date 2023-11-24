4d826930769780198c4741b17d722217e2875d61
Mauro Masciadro <mauro.masciar@hotmail.com>
2023-11-24 20:56:41 -0300

    date and time for database

--- debug/log/Log.java
+++ debug/log/Log.java
@@ -6,30 +6,30 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class Log {
-	public static FileWriter fw = null;
-	public static PrintWriter pw = null;
-	public static LocalDate c = LocalDate.now();
-	public static LocalTime t = LocalTime.now();
+    public static FileWriter fw = null;
+    public static PrintWriter pw = null;
+    public static LocalDate c = LocalDate.now();
+    public static LocalTime t = LocalTime.now();
 
-	public Log(String string) {
-		if (string != null)
-			Loguear(string);
-	}
+    public Log(String string) {
+	if (string != null)
+	    Loguear(string);
+    }
 
-	public static void Loguear(String string) {
-		c = LocalDate.now();
-		t = LocalTime.now();
-		try {
-			fw = new FileWriter("debug.log", true);
-			pw = new PrintWriter(fw);
-			String s = "[" + LocalDate.now().getDayOfMonth() + "/" + LocalDate.now().getMonthValue() + "/" + LocalDate.now().getYear() + " " + LocalTime.now().getHour() + ":" 
-					+ LocalTime.now().getMinute() + ":" + LocalTime.now().getSecond() + "] " + string;
-			pw.println(s);
-			System.out.println(s);
-			fw.close();
-			LogGUI.addText(s);
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
+    public static void Loguear(String string) {
+	c = LocalDate.now();
+	t = LocalTime.now();
+	try {
+	    fw = new FileWriter("debug.log", true);
+	    pw = new PrintWriter(fw);
+	    String s = "[" + LocalDate.now().getDayOfMonth() + "/" + LocalDate.now().getMonthValue() + "/" + LocalDate.now().getYear() + " " + LocalTime.now().getHour() + ":" 
+		    + LocalTime.now().getMinute() + ":" + LocalTime.now().getSecond() + "] " + string;
+	    pw.println(s);
+	    System.out.println(s);
+	    fw.close();
+	    LogGUI.addText(s);
+	} catch (Exception ex) {
+	    ex.printStackTrace();
 	}
+    }
 }
