d067917fd4e2730eac540bfb55b866e8e2e338d9
Mauro Masciadro <mauro.masciar@hotmail.com>
2023-11-27 23:59:06 -0300

    funcion para pasar de segundos a horas:minutos:segundos

---
+++ src/datetime/Utils.java
@@ -0,0 +1,53 @@
+package datetime;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Utils {
+    public static String getDate() {
+	String date;
+	date = LocalDate.now().getYear() + "-" + LocalDate.now().getMonthValue() + "-" + LocalDate.now().getDayOfMonth();
+	return date;
+    }
+
+    public static String getDateTime() {
+	String time, sHour, sMinute, sSecond;
+	int hour, minute, second;
+	hour = LocalTime.now().getHour();
+	minute = LocalTime.now().getMinute();
+	second = LocalTime.now().getSecond();
+
+	if(second < 10) sSecond = "0" + second;
+	else sSecond = String.valueOf(second);
+
+	if(minute < 10) sMinute = "0" + minute;
+	else sMinute = String.valueOf(minute);
+
+	if(hour < 10) sHour = "0" + hour;
+	else sHour = String.valueOf(hour);
+
+	time = LocalDate.now().getYear() + "-" + LocalDate.now().getMonthValue() + "-" + LocalDate.now().getDayOfMonth() + " " + sHour + ":" + sMinute + ":" + sSecond;
+	return time;
+    }
+
+    public static String getTotalHoursFromSeconds(int seconds, boolean withSeconds) {
+	String sHour, sMinute, sSecond;
+	int seconds_final = 0;
+	int minutes = seconds / 60;
+	seconds_final = seconds % 60;
+	int minutes_final = minutes % 60;
+	int hours_final = minutes / 60;
+
+	if(seconds_final < 10) sSecond = "0" + seconds_final;
+	else sSecond = String.valueOf(seconds_final);
+
+	if(minutes_final < 10) sMinute = "0" + minutes_final;
+	else sMinute = String.valueOf(minutes_final);
+
+	if(hours_final < 10) sHour = "0" + hours_final;
+	else sHour = String.valueOf(hours_final);
+
+	if(withSeconds) return sHour + "h " + sMinute + "m " + sSecond + "s";
+	else return sHour + "h " + sMinute + "m";
+    }
+}
--- src/log/Log.java
+++ src/log/Log.java
@@ -12,18 +12,35 @@ public class Log {
     public static LocalTime t = LocalTime.now();
 
     public Log(String string) {
-	if (string != null)
+	if(string != null) {
 	    Loguear(string);
+	}
     }
 
     public static void Loguear(String string) {
 	c = LocalDate.now();
 	t = LocalTime.now();
+
+	String sHour, sMinute, sSecond;
+	int hour, minute, second;
+	hour = LocalTime.now().getHour();
+	minute = LocalTime.now().getMinute();
+	second = LocalTime.now().getSecond();
+
+	if(second < 10) sSecond = "0" + second;
+	else sSecond = String.valueOf(second);
+
+	if(minute < 10) sMinute = "0" + minute;
+	else sMinute = String.valueOf(minute);
+
+	if(hour < 10) sHour = "0" + hour;
+	else sHour = String.valueOf(hour);
+
 	try {
 	    fw = new FileWriter("debug.log", true);
 	    pw = new PrintWriter(fw);
-	    String s = "[" + LocalDate.now().getDayOfMonth() + "/" + LocalDate.now().getMonthValue() + "/" + LocalDate.now().getYear() + " " + LocalTime.now().getHour() + ":" 
-		    + LocalTime.now().getMinute() + ":" + LocalTime.now().getSecond() + "] " + string;
+	    String s = "[" + LocalDate.now().getDayOfMonth() + "/" + LocalDate.now().getMonthValue() + "/" + LocalDate.now().getYear() + " " + sHour + ":" 
+		    + sMinute + ":" + sSecond + "] " + string;
 	    pw.println(s);
 	    System.out.println(s);
 	    fw.close();
